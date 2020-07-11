fcf068c828dde7176ba277b591eb2812082f65bd
EC2 Default User <ec2-user@ip-172-31-34-161.us-east-2.compute.internal>
2020-07-11 11:35:33 +0000

    abs paths

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-sudo docker build -t courseubc:latest .
+sudo docker build -t courseubc:latest /home/ec2-user/get-me-in-a-course
 sudo docker run -t -v /home/ec2-user/get-me-in-a-course/courses.txt:/usr/src/app/courses.txt courseubc:latest
