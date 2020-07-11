b2187c5d3c03953728a1ce60f7aa8415c1eb31ec
EC2 Default User <ec2-user@ip-172-31-34-161.us-east-2.compute.internal>
2020-07-11 11:22:55 +0000

    using full path instead of pwd

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 sudo docker build -t courseubc:latest .
-sudo docker run -t -v $(pwd)/courses.txt:/usr/src/app/courses.txt courseubc:latest
+sudo docker run -t -v /home/ec2-user/get-me-in-a-course/courses.txt:/usr/src/app/courses.txt courseubc:latest
